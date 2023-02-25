02321b7bc8638dd1ed9b960f83c17ba2ebcca4cd
Damien Morcellet <dmorcellet@free.fr>
2023-02-25 21:19:39 +0100

    Added objects source in DefaultXMLIO

--- src/main/java/delta/common/framework/objects/xml/DefaultXMLIO.java
+++ src/main/java/delta/common/framework/objects/xml/DefaultXMLIO.java
@@ -6,6 +6,7 @@ import org.w3c.dom.Element;
 import org.xml.sax.helpers.AttributesImpl;
 
 import delta.common.framework.objects.data.Identifiable;
+import delta.common.framework.objects.data.ObjectsSource;
 
 /**
  * Default implementation for a XML reader/writer.
@@ -14,6 +15,17 @@ import delta.common.framework.objects.data.Identifiable;
  */
 public class DefaultXMLIO<E extends Identifiable<Long>> implements ObjectXMLWriter<E>,ObjectXMLReader<E>
 {
+  protected ObjectsSource _source;
+
+  /**
+   * Set the objects source.
+   * @param source Source to set.
+   */
+  public void setObjectSource(ObjectsSource source)
+  {
+    _source=source;
+  }
+
   @Override
   public E readObject(Element tag, Long id)
   {
